9b6cdd469ab76d1f0ef9732e03865c49a0ec6b5d
Harleen Kaur Singh <harleenkaurs@student.unimelb.edu.au>
2024-06-02 01:13:34 +1000

    second attempt at nav bar with logo

---
+++ javascript/functions.js
@@ -0,0 +1,17 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "fa-solid fa-xmark" : "fa-solid fa-bars")
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
+
+});
+
